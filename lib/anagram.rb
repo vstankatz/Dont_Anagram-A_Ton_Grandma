b73e7cd7caa12c3a6afc9f8b947f4c3eb2214435
Veronica Stanley-Katz <vstankatz@gmail.com>
2019-12-13 09:23:36 -0800

    add first part of anagram method that passes rspec

---
+++ lib/anagram.rb
@@ -0,0 +1,14 @@
+class Check
+
+  def anagram?(word1, word2)
+    first = word1.split("")
+    second = word2.split("")
+    puts first.sort
+    puts second.sort
+    if first.length != second.length
+      false
+    elsif first.sort === second.sort
+      true
+    end
+  end
+end
