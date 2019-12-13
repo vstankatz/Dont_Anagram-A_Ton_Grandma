2b53d9364efaf169cdc498f0b1b7817b0e772fb9
Veronica Stanley-Katz <vstankatz@gmail.com>
2019-12-13 12:42:42 -0800

    change program to use instance variables

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,9 +1,13 @@
 class Check
 
-  def anagram?(word1, word2)
+  def initialize(word1, word2)
+    @word1 = word1
+    @word2 = word2
+  end
+  def anagram?()
     vowels = ["a", "e", "i", "o", "u", "y"]
-    regex_word1 = word1.gsub(/[^A-Za-z]/, "")
-    regex_word2 = word2.gsub(/[^A-Za-z]/, "")
+    regex_word1 = @word1.gsub(/[^A-Za-z]/, "")
+    regex_word2 = @word2.gsub(/[^A-Za-z]/, "")
 
     first = regex_word1.downcase.split("")
     second = regex_word2.downcase.split("")
@@ -13,19 +17,19 @@ class Check
     antigram_check = (first - second)
     if first.sort === second.sort
       if vowel1_check != vowels
-        return "#{word1} and #{word2} are anagrams!"
+        return "#{@word1} and #{@word2} are anagrams!"
       else
-        return "#{word1} and #{word2} are not words, please enter real words."
+        return "#{@word1} and #{@word2} are not words, please enter real words."
       end
     else
       if antigram_check === first
         if vowel1_check != vowels
-        return "#{word1} and #{word2} are antigrams!"
+        return "#{@word1} and #{@word2} are antigrams!"
         else
-          return "#{word1} and #{word2} are not words, please enter real words."
+          return "#{@word1} and #{@word2} are not words, please enter real words."
         end
       else
-        return "#{word1} and #{word2} are not anagrams."
+        return "#{@word1} and #{@word2} are not anagrams."
       end
     end
   end
