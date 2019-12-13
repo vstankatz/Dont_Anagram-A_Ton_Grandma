02884eb94d47f436258d127ca021223596aa6fe5
Veronica Stanley-Katz <vstankatz@gmail.com>
2019-12-13 12:28:09 -0800

    add words to the output of results instead of true or false booleans.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -4,26 +4,29 @@ class Check
     vowels = ["a", "e", "i", "o", "u", "y"]
     regex_word1 = word1.gsub(/[^A-Za-z]/, "")
     regex_word2 = word2.gsub(/[^A-Za-z]/, "")
+
     first = regex_word1.downcase.split("")
     second = regex_word2.downcase.split("")
+
     vowel1_check = (vowels - first)
     vowel2_check = (vowels - second)
     antigram_check = (first - second)
     if first.sort === second.sort
       if vowel1_check != vowels
-        anagram = true
+        return "#{word1} and #{word2} are anagrams!"
       else
-        anagram = false
+        return "#{word1} and #{word2} are not words, please enter real words."
       end
     else
-      puts antigram_check
       if antigram_check === first
-        antigram = true
+        if vowel1_check != vowels
+        return "#{word1} and #{word2} are antigrams!"
+        else
+          return "#{word1} and #{word2} are not words, please enter real words."
+        end
       else
-        anagram = false
+        return "#{word1} and #{word2} are not anagrams."
       end
     end
-
   end
-
 end
--- spec/anagram_spec.rb
+++ spec/anagram_spec.rb
@@ -5,26 +5,26 @@ describe('#anagram?') do
 
   it('should check to see if two words are anagrams') do
     check = Check.new()
-  expect(check.anagram?("bat", "tab")).to(eq(true))
+  expect(check.anagram?("bat", "tab")).to(eq("bat and tab are anagrams!"))
   end
 
   it('should change all casing to match when comparing words') do
     check = Check.new()
-  expect(check.anagram?("StaRt", "TArTs")).to(eq(true))
+  expect(check.anagram?("StaRt", "TArTs")).to(eq("StaRt and TArTs are anagrams!"))
   end
 
   it('should return false if the words fo not include vowels') do
     check = Check.new()
-  expect(check.anagram?("ghb", "bgh")).to(eq(false))
+  expect(check.anagram?("ghb", "bgh")).to(eq("ghb and bgh are not words, please enter real words."))
   end
 
   it('should see if words are antigrams if they share no letters in common') do
     check = Check.new()
-  expect(check.anagram?("bear", "dog")).to(eq(true))
+  expect(check.anagram?("bear", "dog")).to(eq("bear and dog are antigrams!"))
   end
 
   it('should see if multiple words or phrases are anagrams or antigrams') do
     check = Check.new()
-  expect(check.anagram?("I am Lord Voldemort", "Tom Marvolo Riddle")).to(eq(true))
+  expect(check.anagram?("I am Lord Voldemort", "Tom Marvolo Riddle")).to(eq("I am Lord Voldemort and Tom Marvolo Riddle are anagrams!"))
   end
 end
