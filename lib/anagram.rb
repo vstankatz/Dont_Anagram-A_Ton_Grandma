0d728a32ba07499ff45dacd9d7b1d2f270e0106b
Veronica Stanley-Katz <vstankatz@gmail.com>
2019-12-13 15:33:12 -0800

    add is_a_word?() method.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -33,6 +33,21 @@ class Check
         return "#{@word1} and #{@word2} are not anagrams."
       end
     end
-
   end
+
+    def is_a_word?()
+      letters = @word1.split("")
+      for i in letters do
+        puts i
+        j = letters.index(i)
+        puts j
+        if letters[j] === letters[j+1] && letters[j] === letters[j+2]
+          return "three letters in a row"
+        else
+          return letters
+        end
+
+      end
+    end
+
 end
