242dc515e85efa9ea56c2104b5f944a9a79d8d02
Veronica Stanley-Katz <vstankatz@gmail.com>
2019-12-13 09:29:11 -0800

    add second spec test

--- spec/anagram_spec.rb
+++ spec/anagram_spec.rb
@@ -2,8 +2,14 @@ require ('rspec')
 require ('anagram')
 
 describe('#anagram?') do
+
   it('should check to see if two words are anagrams') do
     check = Check.new()
-  expect(check.anagram?("bat","tab")).to(eq(true))
+  expect(check.anagram?("bat", "tab")).to(eq(true))
+  end
+
+  it('should change all casing to match when comparing words') do
+    check = Check.new()
+  expect(check.anagram?("StaRt", "TArTs")).to(eq(true))
   end
 end
